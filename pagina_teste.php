874eb2618d4c8a0e3d2c933ec56f91dc10b4db10
DanielAlvesMorais <daltencedebora@gmail.com>
2024-09-26 10:21:52 -0300

    #

---
+++ pagina_teste.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Post | Projeto para Web com PHP</title>
+        <link rel="stylesheet" href="lib/bootstrap-4.2.1-dist/css/bootstrap.min.css"></link>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <?php
+                    
+                    ?>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
